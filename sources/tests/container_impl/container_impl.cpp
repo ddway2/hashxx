b4198f4b2ca97ff031d13f655adf2e052a3df6ab
Pierre Salmon <pierre.salmon@expandium.com>
2016-05-02 13:03:14 +0200

    Update UT

--- sources/tests/container_impl/container_impl.cpp
+++ sources/tests/container_impl/container_impl.cpp
@@ -9,12 +9,19 @@
 using namespace hashxx;
 
 struct my_struct {
-	uint64_t value1;
-	uint64_t value2;
+	uint64_t value1 = 1;
+	uint64_t value2 = 2;
 };
 
 struct my_noncopyable_struct {
-	std::unique_ptr<uint64_t>	value1;
+	struct foo {
+		uint64_t value1 = 2;
+	};
+
+	std::unique_ptr<foo>	value1{new foo()};
+
+	my_noncopyable_struct()
+	{}
 };
 
 struct my_emplace_struct {
@@ -51,11 +58,14 @@ BOOST_AUTO_TEST_CASE(create_container_and_get_entry_by_move)
 {
 	container_noncopyable_type c1{1000};
 
+	m2.value1->value1 = 5;
+
 	auto v1 = c1.available_entry(std::move(m2));
 
 	BOOST_CHECK_EQUAL(c1.capacity(), 1000);
 	BOOST_CHECK_EQUAL(c1.available_size(), 999);
 	BOOST_CHECK(v1 != nullptr);
+	BOOST_CHECK_EQUAL(v1->data->value1->value1, 5);
 }
 
 BOOST_AUTO_TEST_CASE(create_container_and_entry_by_inplace)
